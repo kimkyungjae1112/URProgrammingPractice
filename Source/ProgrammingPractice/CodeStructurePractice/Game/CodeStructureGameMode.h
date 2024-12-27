9760d9185deaea26a5741e5edcc5c12a3cbb8153
kimkyungjae1112 <rudwo4861@naver.com>
2024-12-27 23:02:25 +0900

    Init

---
+++ Source/ProgrammingPractice/CodeStructurePractice/Game/CodeStructureGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CodeStructureGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROGRAMMINGPRACTICE_API ACodeStructureGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
